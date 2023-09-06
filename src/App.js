84bca4c9750b2672d850ccc140ffe44c4203cece
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-09-06 11:39:46 +0530

    total price

--- src/App.js
+++ src/App.js
@@ -5,16 +5,36 @@ import ProductList from './ProductList/ProductList';
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
   const onProductAddHandler = (product) => {
     setProducts((pre) => {
       return [...pre,product];
     })
+
+    setTotalPrice((pre) => {
+      return Number(pre)+Number(JSON.parse(product).price);
+    })
   }
+
+  const onProductRemoveHandler = (id) => {
+    const item = products.find(el => JSON.parse(el).id === id);
+    console.log(JSON.parse(item).price);
+    setTotalPrice((pre) => {
+      return Number(pre)-Number(JSON.parse(item).price);
+    })
+
+    setProducts((pre) => {
+      return pre.filter(el => JSON.parse(el).id === id)
+    })
+  }
+
   return (
     <div className="App">
       <h1>Ecommerce Dashboard</h1>
       <ProductForm onProductAdd={onProductAddHandler}/>
-      <ProductList products={products}/>
+      <ProductList products={products} removeProduct={onProductRemoveHandler}/>
+      <p>Total Price: {totalPrice} Rs</p>
     </div>
   );
 }
--- src/ProductList/ProductList.js
+++ src/ProductList/ProductList.js
@@ -3,13 +3,14 @@ import './ProductList.css';
 const ProductList = (props) => {
     const removeHandler = (e) => {
         localStorage.removeItem(e.target.id);
+        props.removeProduct(e.target.id);
         e.target.parentElement.remove();
     }
 
     return (
         <ul className='productUl'>
             {props.products.map((ele) => (
-                <li key={JSON.parse(ele).id}>{JSON.parse(ele).id} {JSON.parse(ele).name} {JSON.parse(ele).price}   <span className="remove" id={JSON.parse(ele).id} onClick={removeHandler}>X</span></li>
+                <li key={JSON.parse(ele).id}>Product ID: {JSON.parse(ele).id}, Product Name: {JSON.parse(ele).name}, Product Price: {JSON.parse(ele).price} Rs  <span className="remove" id={JSON.parse(ele).id} onClick={removeHandler}>X</span></li>
             ))}
         </ul>
     )
