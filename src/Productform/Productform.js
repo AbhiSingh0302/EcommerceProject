c0fdc9d210737332f424b67e4808ab71ace792a3
AbhiSingh0302 <abhimanyusingh0302@gmail.com>
2023-09-06 10:44:45 +0530

    product form completed

--- src/Productform/Productform.js
+++ src/Productform/Productform.js
@@ -1,9 +1,49 @@
+import { useState } from "react";
+
 const ProductForm = () => {
+    const [product, setProduct] = useState({
+        id: "",
+        name: "",
+        price: ""
+    })
+    const onChangeProductID = (e) => {
+        const newId = e.target.value;
+        setProduct((pre) => {
+            return {
+                ...pre,
+                id: newId
+            }
+        })
+    }
+    const onChangeProductName = (e) => {
+        const newName = e.target.value;
+        setProduct((pre) => {
+            return {
+                ...pre,
+                name: newName
+            }
+        })
+    }
+    const onChangeProductPrice = (e) => {
+        const newPrice = e.target.value;
+        setProduct((pre) => {
+            return {
+                ...pre,
+                price: newPrice
+            }
+        })
+    }
+
+    const addProduct = (e) => {
+        e.preventDefault();
+        console.log(product);
+        localStorage.setItem(product.id,JSON.stringify(product))
+    }
     return (
-        <form>
-            <input type="number" placeholder="ProductID"/>
-            <input type="text" placeholder="Product Name"/>
-            <input type="number" placeholder="Product Price"/>
+        <form onSubmit={addProduct}>
+            <input type="number" placeholder="ProductID" value={product.id} onChange={onChangeProductID}/>
+            <input type="text" placeholder="Product Name" value={product.name} onChange={onChangeProductName}/>
+            <input type="number" placeholder="Product Price" value={product.price} onChange={onChangeProductPrice}/>
             <button type="submt">Add Product</button>
         </form>
     )
